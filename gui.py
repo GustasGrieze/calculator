3fc2230a7aba760b7eb368ee55f5a9d90ee8a998
Gerkano <geraldasch@gmail.com>
2022-06-14 20:40:40 +0300

    New minor changes

--- gui.py
+++ gui.py
@@ -5,9 +5,19 @@ window = Tk()
 user_input = StringVar()
 window.geometry("650x300")
 
-field = Entry(window)
-button1 = Button(window, text="1")
+def entry_buttons():
+    field_entry=field.get()
+    result["text"] = field_entry
 
+def plus():
+    pass
+
+def minus():
+    pass
+
+field = Entry(window)
+button1 = Button(window, text="1", command=entry_buttons)
+button1.bind("<KP_1>", lambda event: entry_buttons())
 button2 = Button(window, text="2")
 button3 = Button(window, text="3")
 button4 = Button(window, text="4")
@@ -21,7 +31,7 @@ button0 = Button(window, text="0")
 button_sum = Button(window, text="+")
 button_minus = Button(window, text="-")
 button_equals = Button(window, text="=")
-
+result = Label(window, text="")
 button1.grid(column=1, row=1)
 button2.grid(column=2, row=1)
 button3.grid(column=3, row=1)
@@ -38,5 +48,5 @@ button_minus.grid(column=5, row=3)
 button_equals.grid(column=5, row=4)
 
 field.grid(row=1)
-
+result.grid(row=5)
 window.mainloop()
